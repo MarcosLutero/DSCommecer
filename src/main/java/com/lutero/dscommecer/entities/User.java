c885135fc2aeb8b881d180c9d6f20440d4da0633
Marcos Lutero <lutero_rocha@outlook.com>
2024-11-19 21:05:21 -0300

    payment

--- src/main/java/com/lutero/dscommecer/entities/User.java
+++ src/main/java/com/lutero/dscommecer/entities/User.java
@@ -12,11 +12,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "tb_user")
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -25,15 +24,13 @@ public class User {
 	private String fone;
 	private LocalDate bithDate;
 	private String password;
-	
+
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
-	
-	
-	public User() {
-		
-	}
 
+	public User() {
+
+	}
 
 	public User(Long id, String name, String email, String fone, LocalDate bithDate, String password) {
 		this.id = id;
@@ -44,79 +41,62 @@ public class User {
 		this.password = password;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getFone() {
 		return fone;
 	}
-
 
 	public void setFone(String fone) {
 		this.fone = fone;
 	}
 
-
 	public LocalDate getBithDate() {
 		return bithDate;
 	}
-
 
 	public void setBithDate(LocalDate bithDate) {
 		this.bithDate = bithDate;
 	}
 
-
 	public String getPassword() {
 		return password;
 	}
-
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-	
-	
-
-
-	public void setOrders(List<Order> orders) {
-		this.orders = orders;
-	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -130,8 +110,4 @@ public class User {
 		return Objects.equals(id, other.id);
 	}
 
-	
-	
-	
-	
 }
