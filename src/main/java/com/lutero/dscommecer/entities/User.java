cffc66a322487d2260674720af69fee2e7388836
Marcos Lutero <lutero_rocha@outlook.com>
2024-12-26 18:01:14 -0300

    Domain model User Role, seed

--- src/main/java/com/lutero/dscommecer/entities/User.java
+++ src/main/java/com/lutero/dscommecer/entities/User.java
@@ -2,14 +2,19 @@ package com.lutero.dscommecer.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -30,6 +35,10 @@ public class User {
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 
+	@ManyToMany
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
 	public User() {
 
 	}
@@ -42,7 +51,6 @@ public class User {
 		this.birthDate = birthDate;
 		this.password = password;
 	}
-
 
 	public Long getId() {
 		return id;
@@ -96,6 +104,25 @@ public class User {
 		this.orders = orders;
 	}
 
+	
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
